9d9888b5fe212218edadfe6d924e986843546b97
liangjinyong <liangjy881@gmail.com>
2023-04-27 14:50:13 +0900

    ch01 getting started

---
+++ ch01_getting_started/01_basic_window.py
@@ -0,0 +1,25 @@
+import sys
+import typing
+from PyQt6 import QtCore
+from PyQt6.QtWidgets import QApplication, QWidget
+
+
+class EmptyWindow(QWidget):
+
+    def __init__(self):
+        """Constructor for Empty Window Class"""
+        super().__init__()
+        self.initializeUI()
+
+    def initializeUI(self):
+        """Set up the application."""
+        self.setGeometry(200, 100, 400, 300)
+        self.setWindowTitle('Empty Window in PyQt')
+        self.show()  # Display the window on the screen
+
+
+# Run the program
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = EmptyWindow()
+    sys.exit(app.exec())
